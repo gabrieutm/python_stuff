c564f19a201cf51672bd5f157817a100cb4fee27
gabrieutm <gtmachado08@gmail.com>
2024-02-05 20:50:29 -0300

    discover_game

---
+++ discover_game.py
@@ -0,0 +1,10 @@
+caractere = ord(input('Olá! Digite um número, letra ou simbolo:' ) 
+number = caractere == >=48 and =65 and =97 and =32 and =58 and =91 and =123 and <=126) 
+if number: 
+  print('Você digitou um número.') 
+elif upperletter: 
+  print('Você digitou uma letra maiúscula.') 
+elif lowerletter: 
+  print('Você digitou uma letra minúscula.') 
+elif simb: 
+  print('Você digitou um símbolo.')
