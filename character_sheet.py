b21d92f66a5784035e45df1329f0371fe18351ff
gabrieutm <gtmachado08@gmail.com>
2024-02-09 23:08:30 -0300

    D&D_character_sheet
    initiating a simple D&D character sheet creation

---
+++ character_sheet.py
@@ -0,0 +1,8 @@
+print("Hello, welcome to D&D character sheet creation\nEnter the information of your character in the boxes below.")
+
+characters_name = ()
+characters_race = ()
+characters_class = ()
+
+name = input("What's the name of the character?")
+race = input("What's the character race?")
