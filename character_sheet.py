dddda0df9744ca71e4bfe629ea0c38c949ca6c23
gabrieutm <gtmachado08@gmail.com>
2024-02-13 17:51:44 -0300

    Update character_sheet.py

--- character_sheet.py
+++ character_sheet.py
@@ -1,8 +1,7 @@
 print("Hello, welcome to D&D character sheet creation\nEnter the information of your character in the boxes below.")
 
-characters_name = ()
-characters_race = ()
-characters_class = ()
+character_name = input("What's the name of the character? ")
+character_race = input("What's the character race? ")
+character_class = input("What's the character class? ")
 
-name = input("What's the name of the character?")
-race = input("What's the character race?")
+print(f"Your character's name is {character_name}, his race is {character_race} and his class is {character_class}")
