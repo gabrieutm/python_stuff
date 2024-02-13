3ccf540e1aafdcd907669db0bcb7c7403c74cbb8
gabrieutm <gtmachado08@gmail.com>
2024-02-13 18:24:39 -0300

    adjustments

--- first_GUI.py
+++ first_GUI.py
@@ -1,11 +1,26 @@
 from tkinter import *
 
 window = Tk()
-window.geometry("520x520")
+#window.geometry("520x520")
 window.title("My first GUI program")
 
 icon = PhotoImage(file='d20.png')
 window.iconphoto(True,icon)
-window.config(background="black")
+#window.config(background='black')
+
+photo = PhotoImage(file='dndwallpaper.png')
+
+label = Label(window,text="Hello player",
+              font=('Arial',30,'italic'),
+              fg='black',
+              bg='white',
+              relief=RAISED,
+              bd=5,
+              padx=20,
+              pady=20,
+              image=photo,
+              compound='bottom')
+
+label.pack()
 
 window.mainloop()
